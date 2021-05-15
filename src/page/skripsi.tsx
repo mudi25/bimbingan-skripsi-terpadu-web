aed3897e1206355450dabbfb75ce5ace0345b061
mudi25 <mudiarta.tauda52@gmail.com>
2021-05-15 19:22:39 +0800

    update api

--- src/page/skripsi.tsx
+++ src/page/skripsi.tsx
@@ -160,17 +160,20 @@ export default function SkripsiPage() {
         body: "jadwal konsul hari ini",
         title: "informasi",
       };
-      const response = await fetch("http://localhost:8080/send-message", {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-          "Content-Type": "application/json",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(body), // body data type must match "Content-Type" header
-      });
+      const response = await fetch(
+        "https://bimbingan-api.herokuapp.com/send-message",
+        {
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          mode: "cors", // no-cors, *cors, same-origin
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          headers: {
+            "Content-Type": "application/json",
+          },
+          redirect: "follow", // manual, *follow, error
+          referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(body), // body data type must match "Content-Type" header
+        }
+      );
       console.log(response);
     } catch (error) {
       alert(String(error.message));
